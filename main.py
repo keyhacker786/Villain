1c097c59eddd5a6d006db469969df972f4a43a4f
keyhacker786 <62711069+keyhacker786@users.noreply.github.com>
2023-05-15 00:57:10 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from pwn import *
+print("starting server")
 r = process(['python3 Villain.py'], stdin=PTY, raw=False)
 print(r.recv())
 l = pwn.listen(8888)
 
+print("Listening for connections")
+
 while 1:
   l.wait_for_connection()
+  print("Got connection")
   while l:
     r.sendline(l.recvline())
     print(r.recv())
