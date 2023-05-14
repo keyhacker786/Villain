eb15f0875dedd11a15adade34db8552190246825
keyhacker786 <62711069+keyhacker786@users.noreply.github.com>
2023-05-15 01:17:38 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from pwn import *
 print("starting server")
 
+l = listen(4444)
+
+wait_for_connection()
+
+print(l.recv())
 
 
-r = process(['python3'], stdin=PTY, raw=False)
-print(r.recv())
-l = listen(8888)
 
-print("Listening for connections")
 
-while 1:
-  l.wait_for_connection()
-  print("Got connection")
-  while l:
-    r.sendline(l.recvline())
-    print(r.recv())
+# r = process(['python3'], stdin=PTY, raw=False)
+# print(r.recv())
+# l = listen(8888)
+
+# print("Listening for connections")
+
+# while 1:
+#   l.wait_for_connection()
+#   print("Got connection")
+#   while l:
+#     r.sendline(l.recvline())
+#     print(r.recv())
